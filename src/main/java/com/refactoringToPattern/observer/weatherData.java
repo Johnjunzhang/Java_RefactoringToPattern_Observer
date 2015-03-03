6a427192b3085b3453a243a83fe696893b6e48e9
johnjunzhang <johnjunzhang@gmail.com>
2015-03-03 18:43:55 +0800

    [Jun] eliminate temp fields bad smell

--- src/main/java/com/refactoringToPattern/observer/weatherData.java
+++ src/main/java/com/refactoringToPattern/observer/weatherData.java
@@ -4,17 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class weatherData {
-    private seedingMachine seedingMachine;
-    private reapingMachine reapingMachine;
-    private wateringMachine wateringMachine;
     private List<IMachineObserver> observers;
 
     public weatherData(seedingMachine seedingMachine, reapingMachine reapingMachine, wateringMachine wateringMachine)
     {
-        this.seedingMachine = seedingMachine;
-        this.reapingMachine = reapingMachine;
-        this.wateringMachine = wateringMachine;
-        observers = initMachineObservers();
+        seedingMachineObserver seedingMachineObserver = new seedingMachineObserver(seedingMachine);
+        reapingMachineObserver reapingMachineObserver = new reapingMachineObserver(reapingMachine);
+        wateringMachineObserver wateringMachineObserver = new wateringMachineObserver(wateringMachine);
+        observers = Arrays.asList(seedingMachineObserver, reapingMachineObserver, wateringMachineObserver);
     }
 
     public void MeasurementsChanged(int temp, int humidity, int windPower)
@@ -22,10 +19,4 @@ public class weatherData {
         observers.forEach(observer -> observer.start(temp, humidity, windPower));
     }
 
-    private List<IMachineObserver> initMachineObservers() {
-        seedingMachineObserver seedingMachineObserver = new seedingMachineObserver(seedingMachine);
-        reapingMachineObserver reapingMachineObserver = new reapingMachineObserver(reapingMachine);
-        wateringMachineObserver wateringMachineObserver = new wateringMachineObserver(wateringMachine);
-        return Arrays.asList(seedingMachineObserver, reapingMachineObserver, wateringMachineObserver);
-    }
 }
